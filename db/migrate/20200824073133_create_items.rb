a786efda60ac41b30571d9eac899b7f310d5d802
ninjyasoppy <69029725+ninjyasoppy@users.noreply.github.com>
2020-08-27 14:07:45 +0900

    トップページ表示

--- db/migrate/20200824073133_create_items.rb
+++ db/migrate/20200824073133_create_items.rb
@@ -9,7 +9,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.integer :shipping_fee_status_id
       t.integer :prefecture_id
       t.integer :scheduled_delivery_id
-      t.reference :user, null: false, foreign_key: true
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
