5af9a8c24e36afe22075c484c70c013021d06354
ninjyasoppy <69029725+ninjyasoppy@users.noreply.github.com>
2020-08-28 17:07:40 +0900

    factorybotに”a1”追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,8 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname {Faker::Name.last_name}
     email {Faker::Internet.free_email}
-    password = Faker::Internet.password(min_length: 6)
-    password {password}
+    password { 'a1' + Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
     family_name {Faker::Japanese::Name.last_name}
     first_name {Faker::Japanese::Name.last_name}
