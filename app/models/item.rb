a2d70b60dacbcd1483ff47d80cc1ebb5f7490a7a
ninjyasoppy <69029725+ninjyasoppy@users.noreply.github.com>
2020-08-28 22:03:01 +0900

    出品機能実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,9 @@
 class Item < ApplicationRecord
   has_one_attached :image
-  # extend ActiveHash::Associations::ActiveRecordExtensions
-  # belongs_to_active_hash :category
-  # belongs_to_active_hash :sales_status
-  # belongs_to_active_hash :shipping_fee_status
-  # belongs_to_active_hash :prefecture
-  # belongs_to_active_hash :scheduled_delivery
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :sales_status
+  belongs_to_active_hash :shipping_fee_status
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :scheduled_delivery
 end
