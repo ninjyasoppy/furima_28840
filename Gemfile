9ecc51c55f53934d6b1e0ad4022a932cedcf485c
ninjyasoppy <69029725+ninjyasoppy@users.noreply.github.com>
2020-08-28 21:33:16 +0900

    Active Storageの導入

--- Gemfile
+++ Gemfile
@@ -59,6 +59,8 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'pry-rails'
 gem 'devise'
+gem 'mini_magick'
+gem 'image_processing', '~> 1.2'
 
 group :production do
   gem 'rails_12factor'
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,9 @@
 class Item < ApplicationRecord
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :category
-  belongs_to_active_hash :sales_status
-  belongs_to_active_hash :shipping_fee_status
-  belongs_to_active_hash :prefecture
-  belongs_to_active_hash :scheduled_delivery
+  has_one_attached :image
+  # extend ActiveHash::Associations::ActiveRecordExtensions
+  # belongs_to_active_hash :category
+  # belongs_to_active_hash :sales_status
+  # belongs_to_active_hash :shipping_fee_status
+  # belongs_to_active_hash :prefecture
+  # belongs_to_active_hash :scheduled_delivery
 end
