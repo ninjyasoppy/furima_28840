5c9199623ed7a74ce8f65982bd5ee6696b7ed750
ninjyasoppy <69029725+ninjyasoppy@users.noreply.github.com>
2020-08-28 16:25:49 +0900

    変数に入れてから利用、不要コメントアウト 削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,26 +4,22 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   with_options presence: true do
+    EMAIL = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    PASSWORD = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+    NAME = /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/
+    KANA = /\A[ァ-ヶー－]+\z/
     validates :nickname
     validates :birthday
-    validates :email, uniqueness: {case_sensitive: false},format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
-    validates :password, length: {minimum: 6},format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i}
+    validates :email, uniqueness: {case_sensitive: false},format: {with: EMAIL}
+    validates :password, length: {minimum: 6},format: {with: PASSWORD}
     validates :password_confirmation
-    with_options format: {with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/} do
+    with_options format: {with: NAME} do
       validates :family_name
       validates :first_name
     end
-    with_options format: {with: /\A[ァ-ヶー－]+\z/} do
+    with_options format: {with: KANA} do
       validates :family_name_kana
       validates :first_name_kana
     end
   end
 end
-# validates :nickname, presence: true
-# validates :birthday, presence: true
-# validates :email, presence: true, uniqueness: {case_sensitive: false}, format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
-# validates :password, presence: true, length: {minimum: 6}
-# validates :family_name, presence: true,  format: {with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/} 
-# validates :first_name, presence: true, format: {with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/} 
-# validates :family_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
-# validates :first_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
