dee7754015b7d00367c750c64c516b8436ac423e
ninjyasoppy <69029725+ninjyasoppy@users.noreply.github.com>
2020-08-31 14:52:18 +0900

    faker修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,12 +2,12 @@ FactoryBot.define do
   factory :item do
     name {Faker::Name.name}
     text {Faker::Lorem.sentence}
-    price { '1000'}
-    category_id {'2'}
-    sales_status_id {'2'}
-    shipping_fee_status_id {'2'}
-    prefecture_id {'2'}
-    scheduled_delivery_id {'2'}
+    price {Faker::Number.between(from: 300, to: 9999999)}
+    category_id {Faker::Number.between(from: 2, to: 11)}
+    sales_status_id {Faker::Number.between(from: 2, to: 7)}
+    shipping_fee_status_id {Faker::Number.between(from: 2, to: 3)}
+    prefecture_id {Faker::Number.between(from: 2, to: 48)}
+    scheduled_delivery_id {Faker::Number.between(from: 2, to: 4)}
     association :user
   end
 end
