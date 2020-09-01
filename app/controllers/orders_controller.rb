2d4a027627a50e2169b308dbc43d62738e2e1a87
ninjyasoppy <69029725+ninjyasoppy@users.noreply.github.com>
2020-09-01 16:35:46 +0900

    購入機能実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,7 +8,31 @@ class OrdersController < ApplicationController
     end
   end
 
-  def create
+  def create#number,exp_month,exp_year,cvc,postal_cord,prefecture_id,city,addresses,building,phone_number,item_id
+    @order = Order.new(item_id: order_params[:item_id], user_id: order_params[:user_id])
+    if @order.valid?
+      pay_item
+      @order.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
   end
 
+  private
+
+  def order_params#postal_cord,prefecture_id,city,addresses,building,phone_number,token,item_id
+    @item = Item.find(params[:item_id])
+    params.permit(:item_id,:token).merge(user_id: current_user.id)
+  
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # PAY.JPテスト秘密鍵
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency:'jpy'
+    )
+  end
 end
