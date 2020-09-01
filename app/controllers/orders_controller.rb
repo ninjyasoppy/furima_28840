4babf6192988a17b0cf2b241f23214e3298aaad4
ninjyasoppy <69029725+ninjyasoppy@users.noreply.github.com>
2020-09-01 11:06:19 +0900

    自身の商品購入ページに遷移出来ない

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,9 @@ class OrdersController < ApplicationController
 
   def index
     @item = Item.find(params[:item_id])
+    if current_user.id == @item.user_id
+      redirect_to root_path
+    end
   end
 
   def create
